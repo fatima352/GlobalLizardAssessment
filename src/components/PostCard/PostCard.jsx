6baea0d5b245f05e4e3c421a393ef8fcf3ac7d8b
Fatima Abair <fatima-azahara.abair-naimi@etu.umontpellier.fr>
2025-10-26 17:57:57 +0100

    fix: update date format

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -5,10 +5,12 @@ this component handle only display of posts getted from the ListPosts compnenent
 */
 
 const PostCard = ({id, title, date, author}) => {
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {year: 'numeric',month: 'numeric',day: 'numeric'});
+
   return (
       <div className="postCard">
         <h2 className="title">{title}</h2>
-        <p className="date">{date}</p>
+        <p className="date">{formattedDate}</p>
         <div className="author">
           <img src={author.avatar} alt={author.name} />
            <p>{author.name}</p>
